d831b24cd2eadb5017a97a2dac1655d0f9aa14f4
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-05-21 20:41:44 +0100

    Likert scale mirrored

--- includes/echo_likert_scale.php
+++ includes/echo_likert_scale.php
@@ -1,28 +1,28 @@
 <?php
-function echo_likert_scale($question, $scale_name){
+function echo_likert_scale($question, $scale_name) {
     echo("
     <label class='statement'>$question</label>
     <ul class='likert'>
-      <li>
-        <input type='radio' name='$scale_name' value='5' required='required'>
-        <label>Strongly agree</label>
-      </li>
-      <li>
-        <input type='radio' name='$scale_name' value='4'>
-        <label>Agree</label>
-      </li>
-      <li>
-        <input type='radio' name='$scale_name' value='3'>
-        <label>Neutral</label>
-      </li>
-      <li>
-        <input type='radio' name='$scale_name' value='2'>
-        <label>Disagree</label>
-      </li>
-      <li>
-        <input type='radio' name='$scale_name' value='1'>
-        <label>Strongly disagree</label>
-      </li>
+        <li>
+            <input type='radio' name='$scale_name' value='1'>
+            <label>Strongly disagree</label>
+        </li>
+        <li>
+            <input type='radio' name='$scale_name' value='2'>
+            <label>Disagree</label>
+        </li>
+        <li>
+            <input type='radio' name='$scale_name' value='3'>
+            <label>Neutral</label>
+        </li>
+        <li>
+            <input type='radio' name='$scale_name' value='4'>
+            <label>Agree</label>
+        </li>
+        <li>
+            <input type='radio' name='$scale_name' value='5' required='required'>
+            <label>Strongly agree</label>
+        </li>
     </ul>
     ");
 }
