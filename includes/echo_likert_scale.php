ad01b8a38021e754f87e925f545f801d28781271
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-05-23 04:12:56 +0100

    Likert scale labels fixed

--- includes/echo_likert_scale.php
+++ includes/echo_likert_scale.php
@@ -4,24 +4,19 @@ function echo_likert_scale($question, $scale_name) {
     <label class='statement'>$question</label>
     <ul class='likert'>
         <li>
-            <input type='radio' name='$scale_name' value='1'>
-            <label>Strongly disagree</label>
+            <label><input type='radio' name='$scale_name' value='1'>Strongly disagree</label>
         </li>
         <li>
-            <input type='radio' name='$scale_name' value='2'>
-            <label>Disagree</label>
+            <label><input type='radio' name='$scale_name' value='2'>Disagree</label>
         </li>
         <li>
-            <input type='radio' name='$scale_name' value='3'>
-            <label>Neutral</label>
+            <label><input type='radio' name='$scale_name' value='3'>Neutral</label>
         </li>
         <li>
-            <input type='radio' name='$scale_name' value='4'>
-            <label>Agree</label>
+            <label><input type='radio' name='$scale_name' value='4'>Agree</label>
         </li>
         <li>
-            <input type='radio' name='$scale_name' value='5' required='required'>
-            <label>Strongly agree</label>
+            <label><input type='radio' name='$scale_name' value='5' required='required'>Strongly agree</label>
         </li>
     </ul>
     ");
