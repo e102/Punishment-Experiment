c83a2adda53f67a2a48e248f4094c6b0ca088544
es707 <evys@mail.ru>
2017-06-05 09:51:07 +0100

    Update round_1_instructions.php

--- round_1_instructions.php
+++ round_1_instructions.php
@@ -17,9 +17,10 @@ authenticator::authenticate_access("round_1_instructions.php", "round_0_results.
     </p>
     <ul class="list-group">
         <li class="list-group-item">There are three rounds in this part</li>
-        <li class="list-group-item">You start with 20 ECUs</li>
-        <li class="list-group-item">You can contribute ECUs to the common pool</li>
-        <li class="list-group-item">Your ECU's are the sum of your leftover ECUs and the total donated to the common pool times 0.5</li>
+        <li class="list-group-item">You start with 20 ECUs in the first round and what you carry over to the next two rounds depends on your decisions</li>
+        <li class="list-group-item">You can contribute ECUs to the common good or keep them for yourself</li>
+        <li class="list-group-item">Common good doubles all the contributions and sheres them equally amongst all four players.</li>
+        <li class="list-group-item">Your ECUs are the sum of the ECUs you decided to keep for yourself and your share of the ECU from the common good.</li>
     </ul>
 
     <form action='' method='post'>
