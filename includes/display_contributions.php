b96fc7a6bb1350b65000db7f8750accf622f5bf7
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-06-07 20:11:24 +0100

    bigfix

--- includes/display_contributions.php
+++ includes/display_contributions.php
@@ -7,7 +7,7 @@ function display_contributions($player_1_contribution, $player_2_contribution, $
 
     echo("
     <div class = 'display_before_load'>
-        <p>Please wait for other players to make their contributions</p>
+        <p>Please wait for other players to make their choices</p>
     </div>
     
     <div class='display_after_load' style='display:none'>
